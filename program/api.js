7029c3c9c41bed4bf07c2aac024c8149face4b57
nerdfiles <nerdfiles@gmail.com>
2017-05-27 10:48:27 -0500

    Some conventions never die

--- program/api.js
+++ program/api.js
@@ -2,16 +2,16 @@
 var role = require('./role')()
 function api (options) {
   var seneca = this
-  seneca.add('role:api,cmd:noop', role.noop)
-  // seneca.add('role:api,cmd:authenticateUser', role.authenticateUser)
-  seneca.add('role:entity,cmd:save,name:person', role.personSave)
-  seneca.add('role:api,cmd:people', role.peopleGet)
-  seneca.add('role:api,cmd:person', role.personGet)
-  seneca.add('role:api,cmd:person/create', role.personCreate)
-  seneca.add('role:api,cmd:person/remove', role.personRemove)
-  seneca.add('role:api,cmd:person/update', role.personUpdate)
-  seneca.add('role:api,cmd:person/update/inline', role.personInlineUpdate)
-  seneca.add('role:api,cmd:search', role.simpleSearch)
-  seneca.add('role:api,cmd:search/advanced', role.advancedSearch)
+  seneca.add('role:api,cmd:noop                 ', role.noop)
+  // seneca.add('role:api,cmd:authenticateUser  ', role.authenticateUser)
+  seneca.add('role:entity,cmd:save,name:person  ', role.personSave)
+  seneca.add('role:api,cmd:people               ', role.peopleGet)
+  seneca.add('role:api,cmd:person               ', role.personGet)
+  seneca.add('role:api,cmd:person/create        ', role.personCreate)
+  seneca.add('role:api,cmd:person/remove        ', role.personRemove)
+  seneca.add('role:api,cmd:person/update        ', role.personUpdate)
+  seneca.add('role:api,cmd:person/update/inline ', role.personInlineUpdate)
+  seneca.add('role:api,cmd:search               ', role.simpleSearch)
+  seneca.add('role:api,cmd:search/advanced      ', role.advancedSearch)
 }
 module.exports = api
