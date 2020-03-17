a91371ed05a668adfcfd323edd530c7717fc768f
nerdfiles <nerdfiles@gmail.com>
2020-03-17 11:27:03 -0500

    Update package security. Use cheerio to select #random_word

--- program/command/__noop__.js
+++ program/command/__noop__.js
@@ -1,14 +1,17 @@
 // ./program/command/__noop__.js
 const request = require('sync-request');
 const hal = require('hal');
+const cheerio = require('cheerio')
 module.exports = {
   initialize: (args, done) => {
     //var url = 'https://randomwordgenerator.com/fake-word.php';
     var url = 'https://randomword.com/';
     var response = request('GET', url);
+    var contents = response.body.toString('utf-8');
+    const $ = cheerio.load(contents);
     var output = {
       name : 'noop',
-      data : response.body.toString('utf-8')
+      data : $('#random_word').html() 
     };
     var resource = new hal.Resource(output, '/noop');
     resource.link('test', '/noop/test');
